d6b7ff388b0d9fb509d80bdbb022b165b82f25a2
wenhao <wh.ho@outlook.com>
2020-07-22 01:06:48 +0800

    Refactor coding
    Signed-off-by: wenhao <wh.ho@outlook.com>

--- DemoGpsRunning/LocationsStorage.swift
+++ DemoGpsRunning/LocationsStorage.swift
@@ -12,40 +12,47 @@ class LocationStorage {
 
   static let shared = LocationStorage()
   
-  private let workDirectoryURL = FileManager.default.temporaryDirectory.appendingPathComponent("locations", isDirectory: true)
+  private let file: URL?
+  
+  private var outputStream: OutputStream?
   
   init() {
-    do {
-      var isDir: ObjCBool = false
-      if FileManager.default.fileExists(atPath: workDirectoryURL.path, isDirectory: &isDir) && isDir.boolValue {
-        try FileManager.default.removeItem(at: workDirectoryURL)
-      }
-      
-      try FileManager.default.createDirectory(at: workDirectoryURL, withIntermediateDirectories: true, attributes: nil)
-    } catch let error as NSError {
-      print("Unable to create directory", error)
-    }
+    let timestamp = Date().timeIntervalSince1970
+    file = FileManager.default.temporaryDirectory.appendingPathComponent("\(timestamp)")
+  }
+  
+  deinit {
+    outputStream?.close()
   }
   
   func write(location: Location) {
-    let encoder = JSONEncoder()
-    let timestamp = location.date.timeIntervalSince1970
-    let fileURL = workDirectoryURL.appendingPathComponent("\(timestamp)")
+    if let file = self.file, outputStream == nil {
+      outputStream = OutputStream.init(toFileAtPath: file.path, append: true)
+      outputStream?.open()
+    } else {
+      let data = ",".data(using: .ascii)!
+      outputStream?.write(data.map{ $0 }, maxLength: data.map{ $0 }.count)
+    }
     
     do {
+      let encoder = JSONEncoder()
       let data = try encoder.encode(location)
-      try data.write(to: fileURL)
+      outputStream?.write(data.map{ $0 }, maxLength: data.map{ $0 }.count)
     } catch let error as NSError {
       print("Write data failure", error)
     }
   }
   
   func read() -> [Location] {
+    guard file != nil else {
+      return []
+    }
+    
     let decoder = JSONDecoder()
     var result = [Location]()
     
     do {
-      let urls = try FileManager.default.contentsOfDirectory(at: workDirectoryURL, includingPropertiesForKeys: nil, options: [])
+      let urls = try FileManager.default.contentsOfDirectory(at: file!, includingPropertiesForKeys: nil, options: [])
       for url in urls {
         if let text = FileManager.default.contents(atPath: url.path) {
           let location = try decoder.decode(Location.self, from: text)
